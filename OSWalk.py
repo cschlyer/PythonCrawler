3387e3e99f6f15472f5f1b6f7a938985b19973d9
Chris Schlyer <cschlyer@vf_06_041.vectorform.com>
2014-05-22 13:43:33 -0700

    actually made it work

--- OSWalk.py
+++ OSWalk.py
@@ -1,6 +1,6 @@
 #python code
 
-import os, sys
+import os, sys, string, re
 
 # Open a file
 initpath = str(sys.argv[1])
@@ -8,42 +8,20 @@ dirs = os.listdir( initpath )
 
 print "all files in the directory" + initpath
 print "*" * 20
-maxwords = []
+maxword = ""
 maxlength = 0
 for root, dirs, files in os.walk(initpath):
 	for filename in files:
 		if filename.endswith(".m") or filename.endswith(".h"):
 			f = open(os.path.join(root, filename),'r')
-			string = f.read()
-			words = string.split()
+			stringToRead = f.read()
+			new_string = re.sub('[^a-zA-Z0-9\n]', ' ', stringToRead)
+			words = new_string.split()
+			#words = ''.join(c for c in stringToRead if c.isalnum() or c.isspace()).split()
 			for word in words:
 				if len(word) > maxlength:
 					maxlength = len(word)
-					maxwords = [ word ]
-				elif len(word) == maxlength:
-					maxwords.append(word)
-				print word
+					maxword = word
+				#print word
 			f.close()
-		print maxwords
-
-
-
-
-
-
-''' steps to make this work:
-	
-	1. Enter in a directory in terminal through an argument
-		- probably like: cschlyer$ python testing.py -/directory/subdirectory/someDirectory
-	2. Iterate through all files, looking for a .h or .m file
-		-  this could get complicated... recursive function?
-
-	3. Once a file is found, all text is copied into a buffer (?) that the script can read
-	4. All lines are read, looking for the longest variable / method name
-		- probably look for continuous string... no whitespace or special characters
-	5. When serching, store the whole string as a variable
-		- add each new, non-special character to the variable
-	6. Compare new word to length of the longest name
-		- if two are the same, should store in an array
-		- if new one is bigger, it becomes the longest string
-		- if smaller, ignore and move on '''
+print maxword
